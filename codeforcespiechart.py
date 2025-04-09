c25b7ae560cbc76a51d8e10715371b1b3ec41f32
vicfred <git@vicfred.dev>
2025-04-09 15:16:49 -0700

    Update Codeforces

--- codeforcespiechart.py
+++ codeforcespiechart.py
@@ -4,7 +4,11 @@ import numpy as np
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'red', 'orange', 'purple', 'blue', 'cyan', 'green'
 colors = ('#ff0000', '#ffb331', '#773eff', '#2667f2', '#3afeee', '#00c911')
-sizes = [650-1+1, 2836-654+1, 5388-2854+1, 12893-5406+1, 21102-12934+1, 34189-21157+1]
+# Color changes pages: 1,2,4,5,14,26,72,131,228
+# May 31, 2021
+# sizes = [650-1+1, 2836-654+1, 5388-2854+1, 12893-5406+1, 21102-12934+1, 34189-21157+1]
+# April 09, 2025
+sizes = [674-1+1, 2741-674+1, 5115-2741+1, 14363-5115+1, 26185-14363+1, 45558-26185+1]
 #explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 #explode = (0.5, 0.3, 0.2, 0, 0, 0, 0, 0)
 
